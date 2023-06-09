fb4eb51462582dca5f4217f8e5c7bff1678286e0
Amanda Calado <amandabcalado@icloud.com>
2023-06-09 19:33:24 -0300

    add imagens

--- selfcheck/App.js
+++ selfcheck/App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import { Image } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start wor on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Image source={require('./assets/logo-principal.png')} />
+
+      <View style={styles.textoInicio}>
+        <Text style={styles.heading}>Self Check</Text>
+        <Text style={styles.subheading}>
+          “No Egito, as bibliotecas eram chamadas  ''Tesouro dos remédios da alma''. De fato é nela que se cura a ignorância, a mais perigosa das enfermidades e a origem de todas as outras.”
+          ― Jacque Bossuet
+        </Text>
+      </View>
+
+      <TouchableOpacity style={styles.buttonContainer} onPress={() => { }}>
+        <Text style={styles.buttonText}>Faça seu Check-in</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
@@ -15,6 +33,33 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+  },
+  textoInicio: {
+    alignItems: 'center',
+    marginBottom: 20,
+    gap: 30,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'normal',
+    color: '#004A8D',
+
+  },
+  subheading: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: '#000000', // Define a cor do texto como preto
+  },
+  buttonContainer: {
+    backgroundColor: '#004A8D',
+    padding: 20,
+    borderRadius: 30,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
